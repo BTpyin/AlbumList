7a4757665a5bb8e0d77f366b6ae631059717b344
Bowie Tso <bowie.tso.p.y@gmail.com>
2023-03-28 20:02:41 +0800

    project init

---
+++ AlbumList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumList
+//
+//  Created by Bowie Tso on 28/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
